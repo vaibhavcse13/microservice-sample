2a887a693b6db07a8196e846dc5b8d9ebf08dc42
Vaibhav Shukla <cse.vaibhavshukla@gmail.com>
2021-03-27 22:12:53 +0530

    Spring Cloud API Gateway integration

--- account-management-service/src/main/java/com/vaibhav/accountmanagementservice/controller/AccountManagement.java
+++ account-management-service/src/main/java/com/vaibhav/accountmanagementservice/controller/AccountManagement.java
@@ -3,9 +3,8 @@ package com.vaibhav.accountmanagementservice.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/account")
+@RestController
 public class AccountManagement {
-
 
 
     @GetMapping("/status/check")
--- users-service/src/main/java/com/vaibhav/usersservice/controller/UsersController.java
+++ users-service/src/main/java/com/vaibhav/usersservice/controller/UsersController.java
@@ -1,5 +1,7 @@
 package com.vaibhav.usersservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status/check")
     public String staus() {
-        return "Woooo Running";
+        return "Woooo Running" + environment.getProperty("local.server.port");
     }
 
 }
